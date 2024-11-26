80c2fbcbf64efac521d3c0d796eeb334305ee681
MAOZ <maooz4426@gmail.com>
2024-11-26 20:12:43 +0900

    envをセットアップ

--- server/go.mod
+++ server/go.mod
@@ -14,6 +14,7 @@ require (
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- server/main.go
+++ server/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gocraft/dbr/v2"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/maooz4426/usermanager/infra/wire"
 	"github.com/maooz4426/usermanager/lib/usermanage"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("can not read env: %v", err)
+	}
+
+	mysqlUser := os.Getenv("MYSQL_USER")         // "user"
+	mysqlPassword := os.Getenv("MYSQL_PASSWORD") // "password"
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE") // "db"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(usermanage_db:3306)/%s?parseTime=true",
+		mysqlUser,
+		mysqlPassword,
+		mysqlDatabase,
+	)
+
 	e := echo.New()
-	conn, err := dbr.Open("mysql", "user:password@tcp(usermanage_db:3306)/db", nil)
+	conn, err := dbr.Open("mysql", dsn, nil)
 	if err != nil {
 		panic(err)
 	}
